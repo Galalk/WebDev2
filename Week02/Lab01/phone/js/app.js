67af78936eab3f0556a0e00df174454a9cd324e2
Galalk <galalkandil@live.co.uk>
2026-02-27 15:49:37 +0000

    Website Development 2 Labs

--- Week02/Lab01/phone/js/app.js
+++ Week02/Lab01/phone/js/app.js
@@ -1,18 +1,19 @@
+const callStatus = document.querySelector("#callStatus");
 const btnA = document.querySelector("#acceptButton");
 btnA.addEventListener("click", (evt) => {
-    btnA.textContent = ("Call Accepted")
+    callStatus.textContent = ("Call Accepted")
     btnA.classList.add("accepted")
 })
 
 const btnD = document.querySelector("#declineButton");
 btnD.addEventListener("click", (evt) => {
-    btnD.textContent = ("Call Declined")
+    callStatus.textContent = ("Call Declined")
     btnD.classList.add("declined")
 })
 
 const btnM = document.querySelector("#messageButton");
 btnM.addEventListener("click", (evt) => {
-    btnM.textContent = ("Sending Message...")
+    callStatus.textContent = ("Sending Message...")
     btnM.classList.add("message")
     setTimeout(() => {
     alert("I will call you later"); // Show alert after update
